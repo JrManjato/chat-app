866d9ad40b1aee8eac63fd00f3dff008b9bb601d
Judicael <judi.randriamanjato@gmail.com>
2023-05-09 04:55:08 +0300

    wip: implement protected route

--- src/pages/chat.tsx
+++ src/pages/chat.tsx
@@ -1,6 +1,13 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import {useRouter} from "next/router";
 
 function ChatHomePage() {
+    const router = useRouter();
+
+    useEffect(() => {
+       !localStorage.getItem('userInfo') && router.push("/login");
+    }, [])
+
     return (
          <>HomePage for the chat</>
     );
