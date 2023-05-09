7042fdef93166d981908fae0cf8f94f69ed9748b
Judicael <judi.randriamanjato@gmail.com>
2023-05-09 04:51:04 +0300

    feat: store useInfo in the localStorage

---
+++ src/pages/chat.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function ChatHomePage() {
+    return (
+         <>HomePage for the chat</>
+    );
+}
+
+export default ChatHomePage;
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {useForm} from 'react-hook-form';
+import {useRouter} from "next/router";
 
 type FormData = {
     name: string;
@@ -8,26 +9,35 @@ type FormData = {
 };
 
 function Form() {
-    const {register, handleSubmit, reset, formState: { errors }} = useForm<FormData>();
+    const {register, handleSubmit, reset, formState: {errors}} = useForm<FormData>();
+    const router = useRouter();
 
-    function onSubmit(data) {
-        console.log(data);
+    function onSubmit(userInfo: FormData) {
+        localStorage.setItem('userInfo', JSON.stringify(userInfo));
+        router.push("/chat")
         reset();
     }
+
+    useEffect(() => {
+        localStorage.getItem('userInfo') && router.push("/chat");
+    }, [])
 
     return (
         <form onSubmit={handleSubmit(onSubmit)} className='login_form'>
             <label htmlFor="name">Name:</label>
             <input type="text" id="name" name="name" {...register('name', {required: true})} />
-            {errors.name && errors.name.type === "required" && <span className='error_message'>Name is a required field</span>}
+            {errors.name && errors.name.type === "required" &&
+                <span className='error_message'>Name is a required field</span>}
 
             <label htmlFor="email">Email:</label>
             <input type="email" id="email" name="email" {...register('email', {required: true})}/>
-            {errors.email && errors.email.type === "required" && <span className='error_message'>Email is a required field</span>}
+            {errors.email && errors.email.type === "required" &&
+                <span className='error_message'>Email is a required field</span>}
 
             <label htmlFor="password">Password:</label>
             <input type="password" id="password" name="password" {...register('password', {required: true})}/>
-            {errors.password && errors.password.type === "required" && <span className='error_message'>Password is a required field</span>}
+            {errors.password && errors.password.type === "required" &&
+                <span className='error_message'>Password is a required field</span>}
 
             <button type="submit">Submit</button>
         </form>
