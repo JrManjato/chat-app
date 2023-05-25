c59aa0ffd29d8f6227374263dd70f80c7cbbddc9
Judicael <judi.randriamanjato@gmail.com>
2023-05-25 22:24:21 +0300

    feat: implement antdÃ

--- src/pages/chat.tsx
+++ src/pages/chat.tsx
@@ -1,13 +1,26 @@
 import React from 'react';
 import withAuth from "@/common/utils/WithAuth";
-import {getAccessToken, getCookie, removeCookie} from "@/utils/cookie-management";
+import {removeCookie} from "@/utils/cookie-management";
 
 function ChatHomePage() {
     const clearStorage = () => {
-       removeCookie('userInfo');
+        removeCookie('userInfo');
     }
     return (
-        <>HomePage for the chat
+        <>
+            <div className='chat-container container-flex-row'>
+                <div className='channel-container'>
+                    <h1>Channel list</h1>
+
+                </div>
+                <div className='message-container'>
+                    <h1>We have to place the selectedChannel's name here</h1>
+                </div>
+                <div className='channel-info-container'>
+                    <h1>ChannelInfo here</h1>
+                </div>
+            </div>
+            HomePage for the chat
             <button
                 type='button'
                 onClick={() => {
