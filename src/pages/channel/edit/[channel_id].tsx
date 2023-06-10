f1761cd0873350d3a9b9ce5aad27fd4c168f226c
Judicael <judi.randriamanjato@gmail.com>
2023-06-10 07:59:06 +0300

    feat: debug edt channel

--- src/pages/channel/edit/[channel_id].tsx
+++ src/pages/channel/edit/[channel_id].tsx
@@ -1,42 +1,63 @@
 import React, {useEffect, useState} from 'react';
 import {useRouter} from 'next/router';
 import Select from "react-select";
-import {IRestUser, Option} from "@/common/types";
+import {ICreateChannel, IRestUser, Option} from "@/common/types";
 import {useForm} from "react-hook-form";
 import {channelProvider} from "@/provider/channel-provider";
 import {userProvider} from "@/provider/user-provider";
 import makeAnimated from "react-select/animated";
+import {yupResolver} from "@hookform/resolvers/yup";
+import * as Yup from "yup";
+
+const validationSchema = Yup.object().shape({
+    members: Yup.array().of(Yup.number()).required()
+});
 
 const EditChannelPage = () => {
     const router = useRouter();
     const {channel_id} = router.query;
 
-    const [members, setMembers] = useState<IRestUser[]>();
+    const [users, setUsers] = useState<IRestUser[]>([]);
+    const [selectedMembers, setSelectedMembers] = useState<number[]>([]);
 
     useEffect(() => {
         userProvider.getUsers()
             .then((users) => {
-                setMembers(users.data);
+                setUsers(users.data);
             })
             .catch((error) => {
                 console.error(error);
             });
     }, [])
 
-    const animatedComponents = makeAnimated();
 
-    const {handleSubmit, formState: {errors}, setValue} = useForm<IAddMembers>();
+    const { register, handleSubmit, formState: { errors }, setValue } = useForm<ICreateChannel>({
+        resolver: yupResolver(validationSchema)
+    });
 
-    const formattedOptions = members?.map((user: IRestUser) => ({
-        value: user.id,
-        label: user.name
-    }));
 
     interface IAddMembers {
         members: number[];
     }
 
+    const handleCheckboxChange = (userId: number) => {
+        setSelectedMembers((prevMembers: number[]) => {
+            if (prevMembers.includes(userId)) {
+                return prevMembers.filter((memberId) => memberId !== userId);
+            } else {
+                return [...prevMembers, userId];
+            }
+        });
+
+        const arrayOfIDs = selectedMembers.includes(userId)
+            ? selectedMembers.filter((memberId) => memberId !== userId)
+            : [...selectedMembers, userId];
+
+        setValue("members", arrayOfIDs);
+    };
+
     const onSubmit = async (channelInfo: IAddMembers) => {
+        console.log(channelInfo.members)
         channelProvider.addMembers(channel_id.toString(), channelInfo.members).then((newChannel) => router.push(`/channel/${channel_id}`)).catch((err) => console.log(err));;
     }
 
@@ -47,19 +68,19 @@ const EditChannelPage = () => {
             <form onSubmit={handleSubmit(onSubmit)} className='editChannelForm' name="editChannelForm">
 
                 <label htmlFor="password">Members:</label>
-                <Select
-                    onChange={(event) => {
-                        const arrayOfIDs: number[] = [];
-                        event.map((option: Option) => arrayOfIDs.push(option.value));
-                        setValue('members', arrayOfIDs);
-                    }}
-                    closeMenuOnSelect={true}
-                    components={animatedComponents}
-                    isMulti
-                    options={formattedOptions}/>
-                {errors.members && (
-                    <span className='error_message'>{errors.members.message}</span>
-                )}
+                {users?.map((user) => (
+                    <div key={user.id}>
+                        <input
+                            type="checkbox"
+                            id={user.id.toString()}
+                            value={user.id}
+                            checked={selectedMembers?.includes(user.id)}
+                            onChange={() => handleCheckboxChange(user.id)}
+                        />
+                        <label htmlFor={user.id.toString()}>{user.name}</label>
+                    </div>
+                ))}
+                {errors.members && <span className="error_message">{errors.members.message}</span>}
 
                 <button
                     className="editChannelButton"
