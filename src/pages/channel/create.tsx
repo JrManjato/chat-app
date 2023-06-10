73cb6987d23a5a032ae8f97546d37b5f8d24ef7b
Judicael <judi.randriamanjato@gmail.com>
2023-06-10 07:52:34 +0300

    feat: debug create channel

--- src/pages/channel/create.tsx
+++ src/pages/channel/create.tsx
@@ -1,10 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
-import Select from "react-select";
 import * as Yup from "yup";
-import { ICreateChannel, IRestUser, Option } from "@/common/types";
-import makeAnimated from "react-select/animated";
+import { ICreateChannel, IRestUser } from "@/common/types";
 import { channelProvider } from "@/provider/channel-provider";
 import { userProvider } from "@/provider/user-provider";
 import { Button } from "antd";
@@ -17,87 +15,84 @@ const validationSchema = Yup.object().shape({
 });
 
 const CreateChannelPage = () => {
-    const [members, setMembers] = useState<IRestUser[]>();
-    const router = useRouter(); // Change import statement here
+    const [users, setUsers] = useState<IRestUser[]>([]);
+    const [selectedMembers, setSelectedMembers] = useState<number[]>([]);
+    const router = useRouter();
 
     useEffect(() => {
         userProvider.getUsers()
             .then((users) => {
-                setMembers(users.data);
+                setUsers(users.data);
             })
             .catch((error) => {
                 console.error(error);
             });
-    }, [])
-
-    const animatedComponents = makeAnimated();
+    }, []);
 
     const { register, handleSubmit, formState: { errors }, setValue } = useForm<ICreateChannel>({
         resolver: yupResolver(validationSchema)
     });
 
-    const formattedOptions = members?.map((user: IRestUser) => ({
-        value: user.id,
-        label: user.name
-    }));
+    const onSubmit = (channelInfo: ICreateChannel) => {
+        channelProvider.createChannel(channelInfo)
+            .then((newChannel) => router.push(`/channel/${newChannel.data.id}`))
+            .catch((err) => console.log(err));
+    };
 
-    const onSubmit =  (channelInfo) => {
-         channelProvider.createChannel(channelInfo).then((newChannel) => router.push(`/channel/${newChannel.data.id}`)).catch((err) => console.log(err));
-    }
+    const handleCheckboxChange = (userId: number) => {
+        setSelectedMembers((prevMembers: number[]) => {
+            if (prevMembers.includes(userId)) {
+                return prevMembers.filter((memberId) => memberId !== userId);
+            } else {
+                return [...prevMembers, userId];
+            }
+        });
+
+        const arrayOfIDs = selectedMembers.includes(userId)
+            ? selectedMembers.filter((memberId) => memberId !== userId)
+            : [...selectedMembers, userId];
+
+        setValue("members", arrayOfIDs);
+    };
 
     return (
         <>
-            <form onSubmit={handleSubmit(onSubmit)} className='login_form' name="createChannelForm">
+            <form onSubmit={handleSubmit(onSubmit)} className="login_form" name="createChannelForm">
                 <label htmlFor="name">Name:</label>
-                <input
-                    type="text"
-                    id="channelName"
-                    name="channelName"
-                    {...register('name')}
-                />
-                {errors.name && (
-                    <span className='error_message'>{errors.name.message}</span>
-                )}
+                <input type="text" id="channelName" name="channelName" {...register("name")} />
+                {errors.name && <span className="error_message">{errors.name.message}</span>}
 
                 <label htmlFor="email">Type:</label>
-                <select
-                    name="type"
-                    {...register('type')}>
+                <select name="type" {...register("type")}>
                     <option value="public">Public</option>
                     <option value="private">Private</option>
                 </select>
-                {errors.type && (
-                    <span className='error_message'>{errors.type.message}</span>
-                )}
+                {errors.type && <span className="error_message">{errors.type.message}</span>}
 
                 <label htmlFor="password">Members:</label>
-                <Select
-                    onChange={(event) => {
-                        const arrayOfIDs: number[] = [];
-                        event.map((option: Option) => arrayOfIDs.push(option.value));
-                        setValue('members', arrayOfIDs);
-                    }}
-                    closeMenuOnSelect={true}
-                    components={animatedComponents}
-                    isMulti
-                    options={formattedOptions} />
-                {errors.members && (
-                    <span className='error_message'>{errors.members.message}</span>
-                )}
+                {users?.map((user) => (
+                    <div key={user.id}>
+                        <input
+                            type="checkbox"
+                            id={user.id.toString()}
+                            value={user.id}
+                            checked={selectedMembers.includes(user.id)}
+                            onChange={() => handleCheckboxChange(user.id)}
+                        />
+                        <label htmlFor={user.id.toString()}>{user.name}</label>
+                    </div>
+                ))}
+                {errors.members && <span className="error_message">{errors.members.message}</span>}
 
-                <button
-                    className="createChannelButton"
-                    type="submit"
-                >Create Channel</button>
+                <button className="createChannelButton" type="submit">
+                    Create Channel
+                </button>
             </form>
-            <Button
-                type="primary"
-                onClick={async () => router.push('/chat')} // redirect to chat
-            >
-                go back to the chat
+            <Button type="primary" onClick={async () => router.push("/chat")}>
+                Go back to the chat
             </Button>
         </>
-    )
-}
+    );
+};
 
 export default CreateChannelPage;
